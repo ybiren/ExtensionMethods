9c402688e37f5471527a72c4b1d4278957f6baef
ybiren <37359767+ybiren@users.noreply.github.com>
2018-03-14 11:41:16 +0200

    Add files via upload

---
+++ ExtensionMethods/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+	public static class Class1
+	{
+		public static DateTime Add2Month(this DateTime dt)
+		{
+			return dt.AddMonths(2);
+		}
+
+		public static string ToString1(this IEnumerable<int>  l)
+		{
+			string k = "";
+			
+			foreach (var i in l)
+			{
+				k += i.ToString() + ",";
+			}
+			return k;
+		}
+
+		public static IEnumerator<T> Where2<T>(this IEnumerable<T> l)
+		{
+			  for(var i=0 ;i <l.Count(); i++)
+			  {
+				   yield return l.ElementAt(i);
+			  }
+		}
+
+
+	}
+}
